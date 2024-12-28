4cdd960c9ee30cb64dc7b13320c9ad815420b5f5
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-12-27 21:24:32 -0300

    Add tests in main

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "tests_stack.h"
+#include "tests_queue.h"
+#include "tests_list.h"
+#include "tests_map.h"
+#include "tests_tree.h"
+#include "tests_dictionary.h"
 
 
 int main(int argc, char *argv[]) {
+	
+	Stack_TestAll();
+	Queue_TestAll();
+	List_TestAll();
+	Map_TestAll();
+	Tree_TestAll();
+	Dictionary_TestAll();
+	
 	return 0;
 }
