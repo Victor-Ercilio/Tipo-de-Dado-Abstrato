693c7ceb3e66091279a023939f8f3746665d68a6
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-12-29 07:23:13 -0300

    Add dictionary methods

--- headers/dictionary.h
+++ headers/dictionary.h
@@ -4,17 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "map.h"
+#include "e_boolean.h"
 #include "pointers_type.h"
 
-typedef int DictItem;
+typedef MapKey DicKey;
+typedef MapValue DicValue;
 
 typedef struct dictionary {
-	
+	int size;
+	Map *array;
 } * Dictionary;
 
-Dictionary dict(int, int);
+Dictionary dict(int);
+
+unsigned hash(int, int);
 
 void print_dict(Dictionary);
 
+Bool insert_to_dict(DicKey, DicValue, Dictionary);
+
+Bool remove_from_dict(DicKey, Dictionary);
+
+Bool dict_has_key(DicKey, Dictionary);
+
+void del_dict(Dictionary*);
 
 #endif
