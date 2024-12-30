53f4b65887cc2088fa9606511550a2640a031fcc
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-12-30 15:18:27 -0300

    Implemented methods: dict, del_dict

--- bodies/dictionary.c
+++ bodies/dictionary.c
@@ -1,3 +1,28 @@
 #include "dictionary.h"
 
+Dictionary dict(int size){
+	if(size <= 0) return NULL;
+	
+	Dictionary dic = malloc(sizeof(struct dictionary));
+	dic->size = size;
+	dic->array = malloc(sizeof(Map)*size);
+	int i;
+	for(i = 0; i < size; i++)
+		dic->array[i]  = NULL;
 
+	return dic;
+}
+
+void del_dict(Dictionary* dict){
+	if(dict == NULL || *dict == NULL) return;
+	
+	int i;
+	Dictionary temp = *dict;
+	for(i = 0; i < temp->size; i++)
+		del_map(&temp->array[i]);
+	
+	free(temp->array);
+	free(*dict);
+	*dict = NULL;
+	return;
+}
