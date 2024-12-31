e78385897586a53f1bc138e861ecb079987b0d82
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-12-31 08:11:43 -0300

    Add Key const values | Implemented tests for hash

--- tests/bodies/tests_dictionary.c
+++ tests/bodies/tests_dictionary.c
@@ -3,17 +3,32 @@
 #define DICT_SIZE_NEGATIVE -1
 #define DICT_SIZE_ZERO 0
 #define DICT_SIZE_POSITIVE 1 
+#define KEY_SIZE_NEGATIVE -1
+#define KEY_SIZE_ZERO 0
+#define KEY_SIZE_POSITIVE 1 
 
 void Dictionary_TestAll(void){
-	int width = 50;
+	int width = 70;
 	test_header("Testing Dictionary methods\n", width);
-	test("Dict_SizeIsNegativeValue_ReturnsNull", Dict_SizeIsNegativeValue_ReturnsNull, width);
-	test("Dict_SizeIsZero_ReturnsNull", Dict_SizeIsZero_ReturnsNull, width);
-	test("Dict_SizeIsPositiveValue_ReturnsDictionary", Dict_SizeIsPositiveValue_ReturnsDictionary, width);
+	test("Dict_SizeIsNegativeValue_ReturnsNull", 
+		  Dict_SizeIsNegativeValue_ReturnsNull, width);
+	test("Dict_SizeIsZero_ReturnsNull", 
+		  Dict_SizeIsZero_ReturnsNull, width);
+	test("Dict_SizeIsPositiveValue_ReturnsDictionary", 
+		  Dict_SizeIsPositiveValue_ReturnsDictionary, width);
 	
-	test("DelDict_DictIsNUll_ShouldDoNothing", DelDict_DictIsNUll_ShouldDoNothing, width);
-	test("DelDict_DictIsNotNull_ShouldFreeAllStructures", DelDict_DictIsNotNull_ShouldFreeAllStructures, width);
-	// test(,, width);
+	test("DelDict_DictIsNUll_ShouldDoNothing", 
+		  DelDict_DictIsNUll_ShouldDoNothing, width);
+	test("DelDict_DictIsNotNull_ShouldFreeAllStructures", 
+		  DelDict_DictIsNotNull_ShouldFreeAllStructures, width);
+	
+	test("Hash_NegativeValues_ReturnsZero", 
+		  Hash_NegativeValues_ReturnsZero, width);
+	test("Hash_KeyIsPositiveAndSizeIsZero_ReturnsReturnsZero", 
+		  Hash_KeyIsPositiveAndSizeIsZero_ReturnsReturnsZero, width);
+	test("Hash_KeyAndSizeArePositiveNotZeroValues_ReturnsRemainderOfTheKeyByTheSize", 
+		  Hash_KeyAndSizeArePositiveNotZeroValues_ReturnsRemainderOfTheKeyByTheSize, width);
+	// test("", , width);
 }
 
 void Dict_SizeIsNegativeValue_ReturnsNull(void){
@@ -61,6 +76,37 @@ void DelDict_DictIsNotNull_ShouldFreeAllStructures (void){
 	assert(actual == expected);
 }
 
+void Hash_NegativeValues_ReturnsZero(void){
+	unsigned actual;
+	unsigned expected = 0;
+	
+	actual = hash(KEY_SIZE_NEGATIVE, DICT_SIZE_NEGATIVE);
+
+	assert(actual == expected);
+}
+
+void Hash_KeyIsPositiveAndSizeIsZero_ReturnsReturnsZero(void){
+	unsigned actual;
+	unsigned expected = 0;
+	
+	actual = hash(KEY_SIZE_ZERO, DICT_SIZE_ZERO);
+	
+	assert(actual == expected);
+}
+
+void Hash_KeyAndSizeArePositiveNotZeroValues_ReturnsRemainderOfTheKeyByTheSize(void){
+	unsigned actual;
+	unsigned expected = 0;
+	
+	actual = hash(KEY_SIZE_POSITIVE, DICT_SIZE_POSITIVE);
+
+	assert(actual == expected);
+}
+
 #undef DICT_SIZE_NEGATIVE
 #undef DICT_SIZE_ZERO
 #undef DICT_SIZE_POSITIVE
+#undef KEY_SIZE_NEGATIVE
+#undef KEY_SIZE_ZERO
+#undef KEY_SIZE_POSITIVE
+
