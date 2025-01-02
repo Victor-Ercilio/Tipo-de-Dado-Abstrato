906e4d5184d8087c5b4b9ef1de0d6968503cfbfc
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2025-01-02 20:23:06 -0300

    Update main

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "dictionary.h"
+#include "list.h"
+#include "map.h"
+#include "queue.h"
+#include "stack.h"
+#include "tree.h"
+
 #include "tests_stack.h"
 #include "tests_queue.h"
 #include "tests_list.h"
@@ -18,5 +25,15 @@ int main(int argc, char *argv[]) {
 	//Tree_TestAll();
 	Dictionary_TestAll();
 	
+	//Dictionary d = dict(5);
+	//d->array[0] = node_map(1, "Banana", NULL);
+	//d->array[1] = node_map(1, "Laranja", NULL);
+	//d->array[2] = node_map(1, "Pera", NULL);
+	//d->array[3] = node_map(1, "Maca", NULL);
+	//d->array[4] = node_map(1, "Limao", NULL);
+	
+	//print_dict(d);
+
+	//del_dict(&d);
 	return 0;
 }
